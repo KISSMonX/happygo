e42a49f2ca7a6bd4e167d91d1109c02dc3ea38f8
MingH <omonkerman@qq.com>
2019-07-12 22:24:13 +0800

    Fixbug

--- net/tcpclient.go
+++ net/tcpclient.go
@@ -17,19 +17,16 @@ func main() {
 	conn, _ := net.Dial("tcp", "192.168.1.23:4001")
 
 	// 每秒 8000 多帧， 24小时大约 7 亿帧
-	total := 8000 * 24 * 3600
+	total := 9000 * 24 * 3600
 	index := 0
+	errCount := 0
 	// create bars
-	barOK := pb.New(total).Prefix("正常帧数量")
-	barErr := pb.New(total).Prefix("错误帧数量")
-	// start pool
-	pool, err := pb.StartPool(barOK, barErr)
-	if err != nil {
-		panic(err)
-	}
-	// refresh info every second (default 200ms)
+	barOK := pb.New(total)
 	barOK.SetRefreshRate(time.Second)
-	barErr.SetRefreshRate(time.Second)
+	barOK.Prefix("接收正常帧数")
+	// bar will format numbers as (B, KiB, MiB, ...)
+	barOK.SetUnits(pb.U_BYTES)
+	barOK.Start()
 
 	for {
 		if index++; index > total {
@@ -43,16 +40,17 @@ func main() {
 		}
 
 		if len(message) != 13 {
-			log.Println("错误长度: ", len(message))
-			log.Printf("错误内容: %X\n", message)
-			log.Println("***************************************")
-			barErr.Increment()
+			errCount++
+			log.Printf("错误长度: %d, 内容： %X\n", len(message), message)
+			log.Printf("错误帧数： %d  *****************\n", errCount)
+
 			continue
 		}
 
-		barOK.Increment()
+		barOK.Add(len(message))
 	}
 
+	barOK.Finish()
+
 	fmt.Println(total, "帧数据接收完毕，总运行时间： ", time.Since(took))
-	pool.Stop()
 }
